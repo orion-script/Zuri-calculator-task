96278602623bed79a37f73015895253a70133168
Olaniyi John Ayomide <jaolaniyi96@student.lautech.edu.ng>
2022-06-22 18:34:02 +0100

    Zuri calculator task

---
+++ function.js
@@ -0,0 +1,20 @@
+const addition = function (a, b) {
+  return a + b;
+};
+
+const substraction = function (a, b) {
+  return a - b;
+};
+
+const multiplication = function (a, b) {
+  return a * b;
+};
+
+const division = function (a, b) {
+  return a / b;
+};
+
+console.log(addition(6, 2));
+console.log(substraction(6, 2));
+console.log(multiplication(6, 2));
+console.log(division(6, 2));
